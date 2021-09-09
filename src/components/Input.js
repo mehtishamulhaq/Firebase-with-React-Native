f66dc82b706d9a0fe49325fecbfd8f0826817d54
metishamulhaq <m.ehti.shami.777@gmail.com>
2021-09-09 19:54:32 +0500

    some improvements

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,21 +1,20 @@
 import React from 'react'
 import { StyleSheet, TextInput } from 'react-native'
 
-function Input({ style, onChangeText, text, placeholder, keyboardType }) {
+function Input({ style, onChangeText, text, placeholder, ...otherProps }) {
     return (
         <TextInput
             style={{ ...styles.input, ...style }}
             onChangeText={onChangeText}
             value={text}
             placeholder={placeholder}
-            keyboardType={keyboardType}
+            {...otherProps}
         />
     )
 }
 
 Input.defaultProps = {
     placeholder: 'type here...',
-    keyboardType: 'default',
     text: '',
     onChangeText: () => { },
     style: {}
